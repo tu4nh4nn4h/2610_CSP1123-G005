c060a32c766e11f2c7e99b9aa5607bda8e307502
tuan nur hannah <tuanhannah07@gmail.com>
2026-04-17 12:17:09 +0800

    updating and adding details for the organizer_details table

--- user_data.sql
+++ user_data.sql
@@ -2,20 +2,25 @@
 CREATE TABLE users (
     user_id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    username varchar(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
-    password varchar(14) NOT NULL,
+    password VARCHAR(14) NOT NULL,
     role TEXT NOT NULL CHECK(role IN ('user', 'organizer', 'admin'))
 );
 
 CREATE TABLE user_details (
     user_id INT PRIMARY KEY ,
     student_id VARCHAR(10) NOT NULL UNIQUE,
-    full_name VARCHAR(100),
-    phone VARCHAR(20),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
-) ;
+);
 
 CREATE TABLE organizer_details (
+    user_id INT PRIMARY KEY,
+    student_id VARCHAR(10) NOT NULL UNIQUE,
+    phone_number INT NOT NULL,
+    club_body TEXT NOT NULL,
+    position TEXT NOT NULL
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
 
