60bfe3408ba4c156a97eb6c34c7deca58ecc393a
tuan nur hannah <tuanhannah07@gmail.com>
2026-04-24 09:20:03 +0800

    add some code to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import request, session
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
